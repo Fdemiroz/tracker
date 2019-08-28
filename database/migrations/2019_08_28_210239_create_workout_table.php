365cbf732cc4ef1d51403aa6ff907cec71a743c6
Fdemiroz <fdemiroz@live.nl>
2019-08-28 23:27:19 +0200

    setup auth

---
+++ database/migrations/2019_08_28_210239_create_workout_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkoutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workout', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug')->unique();
+            $table->integer('userid');
+            $table->string('title');
+
+            $table->integer('week');
+            $table->integer('totalWeeks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workout');
+    }
+}
